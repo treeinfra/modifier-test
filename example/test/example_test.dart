2d4d4a5a505182674ca8983ed7a69732038f2602
James Aprosail <aprosail@outlook.com>
2024-06-29 20:04:43 +0800

    example and tester

--- example/test/example_test.dart
+++ example/test/example_test.dart
@@ -1,7 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:modifier/modifier.dart';
+import 'package:modifier_test/modifier_test.dart';
 
 void main() {
-  test('placeholder', () {
-    expect(1 + 1, 2);
+  testWidgets('basic function', (t) async {
+    await builder((context) => 'it works'
+        .asText
+        .center
+        .ensureDirection(context)
+        .ensureMedia(context)).pump(t);
+    expect(find.text('it works'), findsOneWidget);
   });
 }
